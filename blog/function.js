9a3f5e9c5cea301242d76ce5d51327f93334d4cb
MatsCornegoor <m.cornegoor@gmail.com>
2021-10-26 23:45:55 +0200

    big update :) added multi user support

--- blog/function.js
+++ blog/function.js
@@ -1,5 +1,47 @@
+// people that have the right to publish posts
+let userPool = ['mats/post.txt', 'meeuwis/post.txt', 'mats/post.txt'];
+
 // init showdown converter
-var conv = new showdown.Converter();
+let conv = new showdown.Converter();
+
+// database array for postMessage.txt files
+let postDatabase = [];
+
+// if link div is on page, start fetching links
+let links = document.getElementById("links");
+if(links != null){
+    fetchLinks(userPool);
+}
+
+async function fetchLinks(urls){
+    
+    Promise.all(
+        urls.map(url => fetch(url).then((response) => response.blob()))
+    ).then((data) => {
+
+        // loop through fetch pool and add data to post data array
+        for(let i = 0; i < data.length; i++){
+            let reader = new FileReader();
+            reader.readAsText(data[i]);
+            reader.onload = function() {
+                // turn file into line separated array
+                let lineArray = reader.result.split(/\r\n|\n/);
+                for(let s = 0; s < lineArray.length; s++){
+                    // split each line into comma separated array and add to post database
+                    postDatabase.push(lineArray[s].split(','));
+                    // if all lines are added to post database, make link list
+                    if(i == data.length -1 && s == lineArray.length -1){
+                        buildPage();
+                    }
+                }
+            }
+            reader.onerror = function() {
+                console.log(reader.error);
+            }
+        }
+
+    });     
+}
 
 // file fetcher based on url
 async function fetchUrl(url) {
@@ -8,63 +50,68 @@ async function fetchUrl(url) {
     return response;
 }
 
-let links = document.getElementById("links");
-if(links != null){
-    fetchUrl('mats/post.txt').then(text => {
 
-        let reader = new FileReader();
-        reader.readAsText(text);
+function buildPage(){
 
-        reader.onload = function() {
-            // turn file into line separated array
-            let resultArray = reader.result.split(/\r\n|\n/);
-            for(let i = 0; i < resultArray.length; i++){
-                console.log(resultArray[i]);
+    // filter database based on date
+    postDatabase.sort(function(date2,date1){
+        a = new Date(date1[0]);
+        b = new Date(date2[0]);
+        if (a>b) return 1;
+        else if (a<b)  return -1;
+        else return 0;
+    });
 
-                // turn lines in comma separated array
-                let lineArray = resultArray[i].split(',');
-                
-                // create link and title elements
-                let link = document.createElement("div")
-                let title = document.createElement("h4");
-                title.textContent = lineArray[2];
+    console.log(postDatabase);
 
-                // add click event listner to link element
-                link.addEventListener("click", function(){ 
+    for(let i = 0; i < postDatabase.length; i++){
+        let link = document.createElement("div")
+        let date = document.createElement("h5");
+        let title = document.createElement("h4");
 
-                    // replace content with html based on file destination
-                    let content = document.getElementById("content");
-                    if(content != null){
-                        fetchUrl(lineArray[3]).then(text => {
+        date.textContent = postDatabase[i][0];
+        title.textContent = postDatabase[i][1];
 
-                            let reader = new FileReader();
-                            reader.readAsText(text);
+        // add click event listner to link element
+        link.addEventListener("click", function(){ 
 
-                            reader.onload = function() {
-                                console.log(reader.result);
-                                // convert markdown to html
-                                content.innerHTML = conv.makeHtml(reader.result);
-                            }
+            // replace content with html based on file destination
+            let content = document.getElementById("content");
+            let contentWrapper = document.getElementById("contentWrapper");
+            if(content != null){
+                fetchUrl(postDatabase[i][3]).then(text => {
 
-                            reader.onerror = function() {
-                                console.log(reader.error);
-                            }
+                    let reader = new FileReader();
+                    reader.readAsText(text);
+
+                    reader.onload = function() {
+                        // console.log(reader.result);
+                        // convert markdown to html
+                        content.innerHTML = conv.makeHtml(reader.result);
+
+                        let close = document.createElement("h4");
+                        close.textContent = "x";
+                        close.classList.add("close");
+                        content.append(close);
+
+                        document.body.scrollTop = document.documentElement.scrollTop = 0;
+
+                        document.querySelector(".close").addEventListener("click", function(){ 
+                            document.getElementById("contentWrapper").style.display = "none";
                         });
                     }
+
+                    reader.onerror = function() {
+                        console.log(reader.error);
+                    }
                 });
-                
-                link.appendChild(title);
-                links.appendChild(link);
             }
-        }
+            contentWrapper.style.display = "block";
+        });
 
-        reader.onerror = function() {
-            console.log(reader.error);
-        }
+        link.appendChild(date);
+        link.appendChild(title);
+        links.appendChild(link);
+    }
 
-    });
 }
-
-
-
-
---
+++ blog/meeuwis/test.md
@@ -0,0 +1,5 @@
+## test
+
+this is a test post 
+
+![Alt text](https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png)
